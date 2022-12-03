5e0dc1669e6420b3a297e4c3484c86329f28d24c
NaveenMadhu <nvenmadhu25@gmail.com>
2022-12-03 20:11:00 +0530

    message 123

--- src/test/java/org/git/main.java
+++ src/test/java/org/git/main.java
@@ -14,4 +14,17 @@ public class main {
 		// TODO Auto-generated method stub
 		System.out.println("3");
 	}
+	public void check3() {
+		// TODO Auto-generated method stub
+		System.out.println("4");
+	}
+	public void check4() {
+		// TODO Auto-generated method stub
+		System.out.println("5");
+	}
+	public void check5() {
+		// TODO Auto-generated method stub
+		System.out.println("6");
+	}
+
 }
