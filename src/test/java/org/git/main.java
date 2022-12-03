d2e4dd44753ac4609c59db40985904de44c5d9b4
NaveenMadhu <nvenmadhu25@gmail.com>
2022-12-03 18:11:13 +0530

    adding 3 methods

---
+++ src/test/java/org/git/main.java
@@ -0,0 +1,17 @@
+package org.git;
+
+public class main {
+
+	public void check() {
+		// TODO Auto-generated method stub
+		System.out.println("1");
+	}
+	public void check1() {
+		// TODO Auto-generated method stub
+		System.out.println("2");
+	}
+	public void check2() {
+		// TODO Auto-generated method stub
+		System.out.println("3");
+	}
+}
